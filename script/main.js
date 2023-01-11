d3cb88b0ef301400f39185aa77ab169482d7eedf
19058261 <Teuku.wiratama@btpn.com>
2023-01-12 00:03:48 +0700

    music

--- script/main.js
+++ script/main.js
@@ -10,12 +10,25 @@ window.addEventListener('load', () => {
     confirmButtonText: 'Klik ini sayang',
     // cancelButtonText: 'No',
   }).then((result) => {
-    if (result.isConfirmed) {
-      document.querySelector('.song').play();
+    // if (result.isConfirmed) {
+      var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+      var source = audioCtx.createBufferSource();
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'music/hbd.mpeg');
+      xhr.responseType = 'arraybuffer';
+      xhr.addEventListener('load', function (r) {
+        audioCtx.decodeAudioData(
+          xhr.response,
+          function (buffer) {
+            source.buffer = buffer;
+            source.connect(audioCtx.destination);
+            source.loop = false;
+          });
+        source.start(0);
+      });
+      xhr.send();
       resolveFetch().then(animationTimeline());
-    } else {
-      resolveFetch().then(animationTimeline());
-    }
+
   });
 });
 
